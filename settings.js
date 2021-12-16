da9f2b157f9bb4857a10727235fe217d5aca7351
KyrSas <sashko.kyrlyk@gmail.com>
2021-12-16 20:52:05 +0200

    12
    1

--- settings.js
+++ settings.js
@@ -1,18 +1,43 @@
 let r = document.getElementById("res").innerHTML;
 var res = +r;
+var checkbox = document.getElementById("checkbox");
 
 
 
-document.getElementById("plus").onclick = function () {
-    let number = document.getElementById("panel").value;
+
+document.getElementById("one").onclick = function () {
+    let number = document.getElementById("one").value;
     let numb = +number;
+    if (checkbox.checked) {
+        numb *= 1;
 
+
+    } else {
+        numb *= -1;
+    };
     document.getElementById("res").innerHTML = res += numb;
 }
-
-document.getElementById("minus").onclick = function () {
-    let number = document.getElementById("panel").value;
+document.getElementById("two").onclick = function () {
+    let number = document.getElementById("two").value;
     let numb = +number;
+    if (checkbox.checked) {
+        numb *= 1;
 
-    document.getElementById("res").innerHTML = res -= numb;
+
+    } else {
+        numb *= -1;
+    };
+    document.getElementById("res").innerHTML = res += numb;
+}
+document.getElementById("three").onclick = function () {
+    let number = document.getElementById("three").value;
+    let numb = +number;
+    if (checkbox.checked) {
+        numb *= 1;
+
+
+    } else {
+        numb *= -1;
+    };
+    document.getElementById("res").innerHTML = res += numb;
 }
